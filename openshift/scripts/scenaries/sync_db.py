efcd6b7090230f7c0b9ec056008f6d1d9e876ed9
vo0doo <kirsanov.bvt@gmail.com>
2020-07-14 16:04:53 +0300

    fix sync db and meta tags

--- openshift/scripts/scenaries/sync_db.py
+++ openshift/scripts/scenaries/sync_db.py
@@ -1,12 +1,13 @@
 import os
 import sys
+import time
 
-vector = sys.argv[1]
-pod = sys.argv[2]
-print(f"{vector} {pod}")
-if vector == "in":
-    os.system(f"oc rsync {pod}:/opt/app-root/src/db.sqlite3 .")
-elif vector == "out":
-    os.system(f"oc rsync . {pod}:/opt/app-root/src --exclude=* --include=db.sqlite3 --no-perms")
-else:
-    print("Usage ./sync_db vector pod")
+time = time.asctime().replace(" ", "-")
+pod = sys.argv[1]
+print(f" {pod}")
+
+os.system(f"cp /home/vo0/projects/blog-osh/pydj-persweb/db.sqlite3 /home/vo0/projects/blog-osh/db.sqlite3[{time}]")
+print("backup complete")
+time.sleep(1)
+os.system(f"oc rsync . {pod}:/opt/app-root/src --exclude=* --include=db.sqlite3 --no-perms")
+print("sync complete")
